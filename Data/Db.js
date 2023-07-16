45cd5682ddb7f9a0ed6a32fd0974382c678846a2
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2023-07-16 20:08:55 +0530

    Fixed all bugs

--- Data/Db.js
+++ Data/Db.js
@@ -1,12 +1,23 @@
 var retArr=[]
+var searchDataArr=[]
 var retString = localStorage.getItem("todoarr");
 retArr = JSON.parse(retString);
 if(retArr==null){
     retArr=[]
 }
-
+var retString = localStorage.getItem("searcharr");
+searchDataArr = JSON.parse(retString);
+if(searchDataArr==null){
+    searchDataArr=[]
+}
 
 function setDbData(Arr){
     let string = JSON.stringify(Arr);
     localStorage.setItem("todoarr", string);
+}
+
+
+function setDbSearchResultsData(Arr){
+    let string = JSON.stringify(Arr);
+    localStorage.setItem("searcharr",string);
 }
